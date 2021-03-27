7793406660a86273bb2914e11980bcac1708bdc0
mysterious-d-raj <81477716+mysterious-d-raj@users.noreply.github.com>
2021-03-28 00:30:22 +0800

    Update FlaskWebhook.py

--- FlaskWebhook.py
+++ FlaskWebhook.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello():
     return "Hello from Python!"
 
+@app.route("/", methods=['GET'])
+def hello(name):
+    return "Hello " + name + " to dialogflow-rest demo webhook!!!"
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
